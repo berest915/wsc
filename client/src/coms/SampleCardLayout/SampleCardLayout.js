91de1518688caf42e8358e3b13d3b7bfb80525d9
elude21933 <43527518+berest915@users.noreply.github.com>
2020-09-26 22:24:16 +0800

    revised mobile-layouts (max-width: 670px) && mobile-components (max-width: 760px)

--- client/src/coms/SampleCardLayout/SampleCardLayout.js
+++ client/src/coms/SampleCardLayout/SampleCardLayout.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {
-	Wrapper
-} from './Styles';
-import {CardContents} from './CardContents'
+import { Wrapper } from './Styles';
+import { CardContents } from './CardContents';
 import CardContent from './CardContent';
+
 const SampleCardLayout = () => {
 	return (
 		<>
 			<Wrapper>
-			{CardContents.map(content => (
-				<CardContent key={content.cardIndex} content={content} />
-			))}
+				{CardContents.map(content => (
+					<CardContent key={content.cardIndex} content={content} />
+				))}
 			</Wrapper>
 		</>
 	);
