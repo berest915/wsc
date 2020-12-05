29b4d2d84e63a77b9ed95632040b03db10fb8952
berest915 <berest915@gmail.com>
2020-12-06 01:05:55 +0800

    push to branch <current>

--- client/src/layouts/navbar/Navbar.js
+++ client/src/layouts/navbar/Navbar.js
@@ -1,58 +1,59 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { Nav, LeftContainer, Span, RightContainer } from './Styles';
-// import AboutModal from '../modal/AboutModal';
-// import Menu from './Menu';
-// import BurgerMenu from './BurgerMenu/BurgerMenu';
-
-// import { useBreakpoint } from '../../IndexContextProvider/breakpoint';
+import { Nav, LeftContainer, RightContainer } from "./Styles";
 
 const Navbar = () => {
-	// const breakpoints = useBreakpoint();
+  // const breakpoints = useBreakpoint();
 
-	// // media-controlled rendered-coms
-	// let isMd;
-	// Object.keys(breakpoints).map(media => {
-	// 	if (media === 'md' && breakpoints[media] === true) {
-	// 		isMd = true;
-	// 	}
-	// 	return null;
-	// });
+  // // media-controlled rendered-coms
+  // let isMd;
+  // Object.keys(breakpoints).map(media => {
+  // 	if (media === 'md' && breakpoints[media] === true) {
+  // 		isMd = true;
+  // 	}
+  // 	return null;
+  // });
 
-	// modal-about
-	const [showAbout, setShowAbout] = useState(false);
+  // modal-about
+  const [showAbout, setShowAbout] = useState(false);
 
-	const handleShowAbout = () => setShowAbout(true);
-	const handleCloseAbout = () => setShowAbout(false);
+  const handleShowAbout = () => setShowAbout(true);
+  const handleCloseAbout = () => setShowAbout(false);
 
-	return (
-		<>
-			<Nav>
-				<LeftContainer>
-					<Link to='/' className='logo-title nav-link'>
-						<i className='logo fab fa-whatsapp' aria-hidden="false"></i>
-						<div className='title-wrap'>
-							<span className='title' >WsChat</span>
-							<span className='caption' >clone*</span>
-						</div>
-					</Link>
-				</LeftContainer>
-				<RightContainer>
-					<Link to='/feature' className='nav-link first-child'>
-						WSCHAT WEB
-					</Link>
-					<Link to='/feature' className='nav-link'>
-						FEATURES
-					</Link>
-					<Link to='/faq' className='nav-link'>
-						FAQ
-					</Link>
-					<Link to='/feature' className='nav-link'>
-						REFERENCE
-					</Link>
+  return (
+    <>
+      <Nav>
+        <LeftContainer>
+          <Link to="/" className="logo-title nav-link">
+            <i className="logo fab fa-whatsapp" aria-hidden="false"></i>
+            <div className="title-wrap">
+              <span className="title">WsChat</span>
+              <span className="caption">clone*</span>
+            </div>
+          </Link>
+        </LeftContainer>
+        <RightContainer>
+          <Link to="/" className="nav-link first-child">
+            HOME
+          </Link>
+          <a
+            className="nav-link"
+            href="https://woocel-v0.web.app"
+            rel="noopener noreferrer"
+            target="_blank"
+            alt="woocel personal project"
+          >
+            WSCHAT WEB
+          </a>
+          <Link to="/feature" className="nav-link">
+            FEATURES
+          </Link>
+          <Link to="/faq" className="nav-link">
+            FAQ
+          </Link>
 
-					{/* {!isMd ? (
+          {/* {!isMd ? (
 						<Menu
 							handleShowAbout={handleShowAbout}
 							handleShowSupport={handleShowSupport}
@@ -65,13 +66,13 @@ const Navbar = () => {
 							handleShowContact={handleShowContact}
 						/>
 					)} */}
-				</RightContainer>
-			</Nav>
-			{/* {showAbout && (
+        </RightContainer>
+      </Nav>
+      {/* {showAbout && (
 				<AboutModal show={showAbout} handleClose={handleCloseAbout} />
 			)} */}
-		</>
-	);
+    </>
+  );
 };
 
 export default Navbar;
